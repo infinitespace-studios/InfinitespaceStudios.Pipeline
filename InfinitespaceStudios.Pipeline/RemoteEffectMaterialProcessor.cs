c618d6e82b78e647c165a78d4e5582141c53f763
Dean Ellis <dellis1972@googlemail.com>
2018-02-14 10:37:59 +0000

    Added RemoteEffect versions of Model processor and the material processor

---
+++ InfinitespaceStudios.Pipeline/RemoteEffectMaterialProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+
+namespace MonoGame.RemoteEffect
+{
+	[ContentProcessor (DisplayName = "Remote Effect Material Processor - Infinitespace Studios")]
+	public class RemoteEffectMaterialProcessor : MaterialProcessor
+	{
+		protected override ExternalReference<CompiledEffectContent> BuildEffect (ExternalReference<EffectContent> effect, ContentProcessorContext context)
+		{
+			return context.BuildAsset<EffectContent, CompiledEffectContent> (effect, typeof(RemoteEffectModelProcessor).Name);
+		}
+	}
+}
---
+++ InfinitespaceStudios.Pipeline/RemoteEffectModelProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+
+namespace MonoGame.RemoteEffect
+{
+	[ContentProcessor (DisplayName = "Remote Effect Model Processor - Infinitespace Studios")]
+	public class RemoteEffectModelProcessor : ModelProcessor
+	{
+		protected override MaterialContent ConvertMaterial (MaterialContent material, ContentProcessorContext context)
+		{
+			var parameters = new OpaqueDataDictionary ();
+			parameters.Add ("ColorKeyColor", ColorKeyColor);
+			parameters.Add ("ColorKeyEnabled", ColorKeyEnabled);
+			parameters.Add ("GenerateMipmaps", GenerateMipmaps);
+			parameters.Add ("PremultiplyTextureAlpha", PremultiplyTextureAlpha);
+			parameters.Add ("ResizeTexturesToPowerOfTwo", ResizeTexturesToPowerOfTwo);
+			parameters.Add ("TextureFormat", TextureFormat);
+			parameters.Add ("DefaultEffect", DefaultEffect);
+
+			return context.Convert<MaterialContent, MaterialContent> (material, typeof(RemoteEffectMaterialProcessor).Name, parameters);
+		}
+	}
+}
